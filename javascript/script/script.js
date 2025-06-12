3f629a08a0ad33299b38bf44f80e5dc21e835432
nathanbc46 <nathanbc46@gmail.com>
2025-06-12 09:14:20 +0700

    feat: add initial HTML, CSS, and TypeScript files with basic functionality

---
+++ javascript/script/script.js
@@ -0,0 +1,4 @@
+const output = document.getElementById('output');
+
+output.textContent = '✅ Yes!';
+output.style.color = 'green';
---
+++ typescript/src/main.ts
@@ -0,0 +1,5 @@
+console.log("Hello, TypeScript By Nathan!");
+
+function sum(a: number, b: number, c: number): number {
+    return a + b + c
+}
